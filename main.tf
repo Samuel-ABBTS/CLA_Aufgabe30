0c037de3ace03dfcf6e4a000a3885f5c70b099c9
Samuel-ABBTS <samuel.buechi@stud.abbts.ch>
2026-02-02 14:47:50 +0100

    Update Azure provider configuration
    Updated Azure provider configuration with new credentials.

--- main.tf
+++ main.tf
@@ -4,7 +4,11 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "..."
+
+  subscription_id = "<your_subscription_id>"
+  client_id       = "f5806fd4-77e4-40aa-aeb8-a64b9cce55c8"
+  client_secret   = "qJX8Q~T5iy.nlVby30xwE-UvB-BIBwjJUgH0PdpX"
+  tenant_id       = "bc2dca8e-97a0-492b-8c03-e0969ace6ad3"
 }
 
 resource "azurerm_resource_group" "rg" {

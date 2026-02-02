f95791b6ec20f586cabd0e8fef240c1cfa908cb3
Samuel-ABBTS <samuel.buechi@stud.abbts.ch>
2026-02-02 14:53:38 +0100

    Remove sensitive Azure credentials from main.tf
    Removed sensitive Azure provider credentials from main.tf.

--- main.tf
+++ main.tf
@@ -4,11 +4,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-
-  subscription_id = "d00e4edc-1cd9-4705-803b-cb95cbe36fb9"
-  client_id       = "f5806fd4-77e4-40aa-aeb8-a64b9cce55c8"
-  client_secret   = "qJX8Q~T5iy.nlVby30xwE-UvB-BIBwjJUgH0PdpX"
-  tenant_id       = "bc2dca8e-97a0-492b-8c03-e0969ace6ad3"
 }
 
 resource "azurerm_resource_group" "rg" {

4a1fa3a17da03a1cc18bb283a083683c811b2405
Samuel-ABBTS <samuel.buechi@stud.abbts.ch>
2026-02-02 14:34:28 +0100

    Add Azure provider and resource group configuration

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
 terraform {
   backend "azurerm" {}
 }
+
+provider "azurerm" {
+  features {}
+  subscription_id = "..."
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = "swedencentral"
+}
